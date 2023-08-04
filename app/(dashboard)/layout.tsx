6def9f2a21ac814786a9ae1aa6245de096f7768a
Trey <treym@linettecourt.com>
2023-08-04 13:10:02 -0400

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
+const DashboardLayout = async ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+
+  const apiLimitCount = await getApiLimitCount();
+
+  return (
+    <div className="h-full reletive">
+     <div className="hidden h-full md:flex 
+     md:w-72 md:flex-col md:fixed md:inset-t-20 bg-gray-900">
+        <Sidebar apiLimitCount={apiLimitCount}/>
+     </div>
+     <main className="md:pl-72">
+        <Navbar />
+        {children}
+     </main>
+    </div>
+  )
+}
+
+export default DashboardLayout
